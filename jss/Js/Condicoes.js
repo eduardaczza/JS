8983741cde1aaaff3b4cb3cab3b21696d3a6a202
eduardaczza <165608058+eduardaczza@users.noreply.github.com>
2024-08-27 15:15:13 -0300

    correções de codigo

---
+++ jss/Js/Condicoes.js
@@ -0,0 +1,6 @@
+var velocidade = 60.5
+console.log(`A velocidade do seu carro é de ${velocidade} km/h`)
+if (velocidade > 60){  //condição simples
+    console.log(`Você ultrapassou o limite de velocidade permitido. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança`)
