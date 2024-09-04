a8283da0e7200458c2f98435e4896db1410c2915
eduardaczza <165608058+eduardaczza@users.noreply.github.com>
2024-09-04 15:52:33 -0300

    edições feitas

---
+++ jss/Alura/Imersão/dados.js
@@ -0,0 +1,13 @@
+let dados = [{
+    titulo: "A Rainha Vermelha",
+    descricao: "A Rainha Vermelha é um romance de fantasia que nos transporta para um mundo dividido por duas castas: os vermelhos, plebeus que vivem em pobreza e opressão, e os prateados, uma elite com poderes sobrenaturais que governa com mão de ferro.", 
+    link: "https://www.amazon.com.br/rainha-vermelha-Victoria-Aveyard/dp/8565765695"
+}, {
+    titulo: "Corte de Espinhos e Rosas",
+    descricao: "Depois de anos sendo escravizados pelas fadas, os humanos conseguiram se libertar e, agora, coexistem com os seres místicos. Cerca de cinco séculos após a guerra que definiu o futuro das espécies, Feyre, filha de um casal de mercadores, é forçada a se tornar uma caçadora para ajudar a família, afundada na pobreza. Após matar uma criatura feérica transformada em lobo, uma criatura bestial surge exigindo uma reparação.",
+    link: "https://www.amazon.com.br/Corte-espinhos-rosas-Vol-especial/dp/6559810720"
+}, {
+    titulo: "De Sangue e Cinzas",
+    descricao: "Prestes a completar 19 anos, Poppy se prepara para sua Ascensão. Quer dizer, Poppy é preparada para sua Ascensão, já que, como tudo mais em sua vida, ela não tem opção senão Ascender. Sendo a Donzela, ela será entregue aos deuses de qualquer forma, seja lá o que isso signifique.",
+    link: "https://www.amazon.com.br/sangue-cinzas-Vol-1/dp/655981002X"
+}]
