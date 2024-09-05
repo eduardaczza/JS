60a7fc6c8df1630d923fe5526895a9c7e297ae61
eduardaczza <165608058+eduardaczza@users.noreply.github.com>
2024-09-05 14:44:37 -0300

    organização do código
    Organizando e dando forma a pesquisa no site

--- jss/Alura/Imersão/app.js
+++ jss/Alura/Imersão/app.js
@@ -1,1 +1,25 @@
-console.log(dados[2])
+function pesquisar() {
+    // Seleciona a seção onde os resultados serão exibidos
+    let section = document.getElementById("resultados-pesquisa");
+  
+    // Inicializa uma string vazia para armazenar o HTML dos resultados
+    let resultados = "";
+  
+    // Itera sobre cada dado da pesquisa e constrói o HTML
+    for (let dado of dados) {
+      // Cria um novo elemento div para cada resultado
+      resultados += `<div class="item-resultado">
+        // Adiciona um título com um link para o dado
+        <h2>
+          <a href="#" target="_blank">${dado.titulo}</a>
+        </h2>
+        // Adiciona uma descrição
+        <p class="descricao-meta">${dado.descricao}</p>
+        // Adiciona um link para mais informações
+        <a href="${dado.link}" target="_blank"> Mais informações</a>
+      </div>`;
+    }
+  
+    // Atualiza o conteúdo da seção com os resultados gerados
+    section.innerHTML = resultados;
+  }
