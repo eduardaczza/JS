57b8a8d01f6bec8efd42777d18be22bd9b7e69f6
eduardaczza <165608058+eduardaczza@users.noreply.github.com>
2024-09-06 16:31:29 -0300

    novos commits

--- jss/Alura/Imersão/app.js
+++ jss/Alura/Imersão/app.js
@@ -2,22 +2,38 @@ function pesquisar() {
     // Seleciona a seção onde os resultados serão exibidos
     let section = document.getElementById("resultados-pesquisa");
   
-    // Inicializa uma string vazia para armazenar o HTML dos resultados
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
+  //se campoP estiver vazio
+    if (campoPesquisa == ""){
+      section.innerHTML = "<p>Resultado não encontrado</p>"
+      return;
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase()
+
     let resultados = "";
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
   
     // Itera sobre cada dado da pesquisa e constrói o HTML
     for (let dado of dados) {
-      // Cria um novo elemento div para cada resultado
-      resultados += `<div class="item-resultado">
-        // Adiciona um título com um link para o dado
-        <h2>
-          <a href="#" target="_blank">${dado.titulo}</a>
-        </h2>
-        // Adiciona uma descrição
-        <p class="descricao-meta">${dado.descricao}</p>
-        // Adiciona um link para mais informações
-        <a href="${dado.link}" target="_blank"> Mais informações</a>
-      </div>`;
+      titulo = dado.titulo.toLowerCase()
+
+      descricao = dado.descricao.toLowerCase()
+
+      tags = dado.tags.toLowerCase()
+
+      if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)){
+        resultados += `<div class="item-resultado">
+      <h2>
+        <a href="#" target="_blank">${dado.titulo}</a>
+      </h2>
+     <p class="descricao-meta">${dado.descricao}</p>
+      <a href="${dado.link}" target="_blank"> Mais informações</a>
+    </div>`;
+
+      }
     }
   
     // Atualiza o conteúdo da seção com os resultados gerados
